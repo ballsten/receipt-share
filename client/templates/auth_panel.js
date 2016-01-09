756ecd5a93cc22b740cbf9ab1ddb26080296d255
Andrew Theaker <andrew@theaker.name>
2016-01-09 23:58:52 +0000

    Login for now submits on enter key press

--- client/templates/auth_panel.js
+++ client/templates/auth_panel.js
@@ -1,15 +1,18 @@
 Template.authPanel.events({
-    'click div.login.button': function(e) {
+    'submit form.login': function(e) {
         Meteor.loginWithPassword($('input.login.username').val(), $('input.login.password').val(), 
         function(err) {
             // TODO: need to display an error message
             console.log('login error ' + err);
         });
     },
+    'click div.login.button': function(e) {
+        $('form.login').submit();
+    },
     'click div.logout.button': function(e) {
         Meteor.logout( 
         function(err) {
-            console.log('login error ' + err);
+            console.log('logout error ' + err);
         });
     }
 })
