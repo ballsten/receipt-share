7a7148bb59a0add8a620b21219acb318a6f872b1
Andrew Theaker <andrew@theaker.name>
2016-01-16 23:25:28 +0000

    tidy up login

--- client/templates/auth_panel.js
+++ client/templates/auth_panel.js
@@ -1,18 +1,19 @@
 Template.authPanel.events({
     'submit form.login': function(e) {
+        e.preventDefault();
         Meteor.loginWithPassword($('input.login.username').val(), $('input.login.password').val(), 
         function(err) {
-            // TODO: need to display an error message
-            console.log('login error ' + err);
+            if(err) console.log('login error ' + err);;
         });
     },
     'click div.login.button': function(e) {
+        console.log('click');
         $('form.login').submit();
     },
     'click div.logout.button': function(e) {
         Meteor.logout( 
         function(err) {
-            console.log('logout error ' + err);
+            if(err) console.log('logout error ' + err);
         });
     }
-})
+});
